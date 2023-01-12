36a62610931214b26e4d25edccc09f3ecafe4424
jokkeiii <joakim.ijas@gmail.com>
2023-01-12 16:08:59 +0200

    init commit

---
+++ course-exercises/basic-template.cpp
@@ -0,0 +1,14 @@
+// x. excersice of round x
+// Short program explanation
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    
+    
+    return 0;
+}
---
+++ course-exercises/round-01/basic-pointers.cpp
@@ -0,0 +1,14 @@
+// 1. excersice of round 1
+// Short programn explanation
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    
+    
+    return 0;
+}
