32f1b177adfe76373516aecb59256a9250c0aba6
jokkeiii <joakim.ijas@gmail.com>
2023-01-13 13:12:56 +0200

    finished basic-pointers

--- course-exercises/round-01/basic-pointers.cpp
+++ course-exercises/round-01/basic-pointers.cpp
@@ -4,35 +4,47 @@
 #include<iostream>
 
 using std::cout;
-using std::cin;
 using std::endl;
+using std::nullptr_t;
 
 int main() {
     
     // int "number" variable and a pointer variable to that "number"
-    int number, *number_p = &number;
+    int *number_p, number;
     
+    number_p = &number;
+
     // assign 7 to the "number" variable through the dereference of pointer variable
     *number_p = 7;
 
     // print the values in both variables
-    cout << "'Number' value is " << number << endl;
-    cout << "'Number_p' value is " << number_p << endl;
+    cout << "' Number_p ' value is " << number_p << endl;
+    cout << "' Number ' value is " << number << endl;
     cout << endl;
 
     // print the address of both variables
-    cout << "'Number' address is " << &number << endl;
-    cout << "'Number_p' address is " << &number_p << endl;
+    cout << "' &Number_p ' value is " << &number_p << endl;
+    cout << "' &Number ' value is " << &number << endl;
+    cout << endl;
+
+    // print dereferences
+    cout << "' &*Number_p ' value is " << &*number_p << endl;
+    cout << "' *&Number ' value is " << *&number << endl;
     cout << endl;
     
     // print the sizes of each variable
-    cout << "'Number' variable size is " << sizeof(number) << " bytes." << endl;
-    cout << "'Number_p' variable size is " << sizeof(number_p) << " bytes." << endl;
+    cout << "' Number_p ' variable size is " << sizeof(number_p) << " bytes." << endl;
+    cout << "' Number ' variable size is " << sizeof(number) << " bytes." << endl;
     cout << endl;
     
     int *second_number_p;
 
-    cout << "'Second_number_p' value is " << second_number_p << endl;
+    cout << "' Second_number_p ' default value is " << second_number_p << endl;
+    cout << endl;
+
+    second_number_p = nullptr;
+
+    cout << "' Second_number_p ' value as nullptr is " << second_number_p << endl;
 
     return 0;
 }
