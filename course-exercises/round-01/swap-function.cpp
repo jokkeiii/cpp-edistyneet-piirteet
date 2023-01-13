afff7fcfc52a7c3adfb40fea72f40baa4438e2f7
jokkeiii <joakim.ijas@gmail.com>
2023-01-13 13:50:19 +0200

    finished swap

--- course-exercises/round-01/swap-function.cpp
+++ course-exercises/round-01/swap-function.cpp
@@ -7,13 +7,36 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void swap(){
+void swap(int *f_one, int *f_two){
 
+    int temp;
+    temp = *f_one;
+    *f_one = *f_two;
+    *f_two = temp;
 
 }
 
 int main() {
     
+    int first = 1, second = 2;
     
+    cout << "first = " << first << ", second = " << second << " before swap 1" << endl;
+    cout << endl;
+
+    swap(&first, &second);
+
+    cout << "first = " << first << ", second = " << second << " after swap 1" << endl;
+    cout << endl;
+
+    int third = 3, fourth = 4;
+
+    cout << "third = " << third << ", fourth = " << fourth << " before swap 2" << endl;
+    cout << endl;
+
+    swap(&third, &fourth);
+
+    cout << "third = " << third << ", fourth = " << fourth << " after swap 2" << endl;
+    cout << endl;
+
     return 0;
 }
