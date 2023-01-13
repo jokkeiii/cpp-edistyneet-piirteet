6f771138444b86ef3f489433eb997efdebf5084c
jokkeiii <joakim.ijas@gmail.com>
2023-01-13 13:28:41 +0200

    created file

---
+++ course-exercises/round-01/swap-function.cpp
@@ -0,0 +1,19 @@
+// 2. exercise of round 1
+// Short program explanation
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void swap(){
+
+
+}
+
+int main() {
+    
+    
+    return 0;
+}
