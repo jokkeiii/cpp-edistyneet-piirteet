7720fdb4d7c37a4c2cb59f2012ba069aeeb965c4
jokkeiii <joakim.ijas@gmail.com>
2023-01-17 11:56:58 +0200

    finished array-swap

--- course-exercises/round-01/array-swap.cpp
+++ course-exercises/round-01/array-swap.cpp
@@ -7,36 +7,88 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void arraySwap(int array[10]){
-    
-    bool use_index;
-    
-    cout << "Use indexing?(true/false)?" << endl;
-    cin >> use_index;
+// Function swaps array's values with pointers
+// and without indexing
+void arraySwap(int *f_array_p) {
 
-    if (use_index)
+    int *temp;
+
+    for (int i = 0; i < 10; i++)
     {
-        for (int i = 0; i < 10; i++)
-        {
-           array[i] = 1;
-        }
-        
+        temp = (f_array_p + i);
+        *temp = 1;
     }
-    
+
 }
 
+//Function swaps array's values with indexing
+void arrayIndexSwap(int f_array[10]) {
+
+    for (int i = 0; i < 10; i++)
+    {
+        f_array[i] = 1;
+    }
+
+}
+
+
 int main() {
-    
+
     // Initializing array with zeroes
-    int array[10] = {0};
+    int array[10] = { 0 };
+    // initialize pointer of array
+    int *array_p = array;
+
+    cout << "\naliohjelma 1 indeksoinnilla\n";
+    cout << "\ntaulukko ennen aliohjelmakutsua 1: ";
 
     // loop to print out every value in array
     for (int i = 0; i < 10; i++)
     {
         cout << array[i] << " ";
     }
+
+
+    // call function to swap array's values
+    arrayIndexSwap(array);
+
+    cout << "\n\ntaulukko aliohjelmakutsun 1 jalkeen: ";
+
+    // loop to print out every value in array
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << " ";
+    }
+
+    cout << "\n";
+
+    // initializes the array back to zeroes
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = 0;
+    }
     
-    arraySwap(&array);
-    
+    cout << "\n\naliohjelma 2 ilman indeksointia\n";
+    cout << "\ntaulukko ennen aliohjelmakutsua 2: ";
+
+    // loop to print out every value in array
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << " ";
+    }
+
+    // call function to swap array's values
+    arraySwap(array_p);
+
+    cout << "\n\ntaulukko aliohjelmakutsun 2 jalkeen: ";
+
+    // loop to print out every value in array
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << " ";
+    }
+
+    cout << "\n\n";
+
     return 0;
 }
